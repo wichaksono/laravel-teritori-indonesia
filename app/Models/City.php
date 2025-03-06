f29f1e29e67be11763c7725d5e310c9472dc10ef
Wakhid Wichaksono <wichaksono@users.noreply.github.com>
2025-03-06 08:48:13 +0700

    Update and rename Models/City.php to app/Models/City.php

---
+++ app/Models/City.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class City extends Model
+{
+    protected $fillable = [
+        'name',
+        'province_id',
+    ];
+
+    public $timestamps = false;
+
+    public function province(): BelongsTo
+    {
+        return $this->belongsTo(Province::class);
+    }
+
+    public function districts(): HasMany
+    {
+        return $this->hasMany(District::class);
+    }
+}
