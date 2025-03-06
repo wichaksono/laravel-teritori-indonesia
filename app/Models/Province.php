fe1ed10072ebf1a0b24d6f83d3eb61c0e351bdaf
Wakhid Wichaksono <wichaksono@users.noreply.github.com>
2025-03-06 08:48:46 +0700

    Update and rename Models/Province.php to app/Models/Province.php

---
+++ app/Models/Province.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Province extends Model
+{
+    protected $fillable = [
+        'name',
+    ];
+
+    public $timestamps = false;
+
+    public function cities(): HasMany
+    {
+        return $this->hasMany(City::class);
+    }
+
+}
